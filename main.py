097039dc882a7af4445c7a9fa7eba185dd82cbc8
Kevin Toun <blackphoenixdigital@gmail.com>
2025-04-15 09:57:35 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
-VERIFY_TOKEN = 'innerg-secret'
+VERIFY_TOKEN = "innerg-secret"
 
 @app.route('/webhook', methods=['GET'])
-def webhook():
-    mode = request.args.get('hub.mode')
-    token = request.args.get('hub.verify_token')
-    challenge = request.args.get('hub.challenge')
+def webhook_verify():
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
 
-    if mode == 'subscribe' and token == VERIFY_TOKEN:
-        print("✅ WEBHOOK VERIFIED")
-        return challenge or '', 200
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        response = make_response(challenge, 200)
+        response.mimetype = "text/plain"
+        return response
     else:
-        return "❌ Forbidden", 403
+        return "Forbidden", 403
